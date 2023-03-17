e6b305765869c0f81e3cd5f2255dbf09ffd3ed6c
juanscarlata2 <56403767+juanscarlata2@users.noreply.github.com>
2023-03-17 10:01:24 -0500

    fix core

--- Examples/example_2/ex2.js
+++ Examples/example_2/ex2.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/handle-request', (req, res) => {
   const json = req.body.config
-  const config = JSON.parse(json) // vulnerable to insecure deserialization
+  const config = JSON.parse(json) 
 
   // Use config object in application logic
   // ...
